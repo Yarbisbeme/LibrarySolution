96684e08472980c13f0d26713776054160837106
yarbisbeme <yarbisbem@gmail.com>
2025-10-11 12:22:17 -0400

    Cambios para el book

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -4,11 +4,11 @@ using Library.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 // Add services to the container
 builder.Services.AddScoped<IBookService, LibroService>();
 builder.Services.AddScoped<ILoanService, PrestamoService>();
+
+//Controladores y los endpoints
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- Library.Common/Dto/ActualizarDevolucion.cs
+++ Library.Common/Dto/ActualizarDevolucion.cs
@@ -1,8 +1,5 @@
 namespace Library.Application.DTOs;
 
-/// <summary>
-/// DTO para actualizar la fecha de devolución de un préstamo
-/// </summary>
 public class ActualizarDevolucionDto
 {
     public DateTime FechaDevolucion { get; set; }
